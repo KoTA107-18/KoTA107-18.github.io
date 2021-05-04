58c8a674eb23ebd246dd0990f732642100b4f1e4
Andi Fauzy Dewantara <andi.fauzy.tif18@polban.ac.id>
2021-05-04 12:21:42 +0700

    Ubah https, bisa Server

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -14,9 +14,9 @@ const RESOURCES = {
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"index.html": "802812dbac50eecee51ba6c682a6aae5",
-"/": "802812dbac50eecee51ba6c682a6aae5",
-"main.dart.js": "7a81e41d2c63dfd16195475e23f7424a",
+"index.html": "94e672078a9470df398599c7a45390c8",
+"/": "94e672078a9470df398599c7a45390c8",
+"main.dart.js": "3856e2b87c02ae30efd009379350d29b",
 "manifest.json": "be75a275105cf347e753af0d81c9c56c",
 "version.json": "e3fc9c2a3090710cf54b08f1564fb1f4"
 };
