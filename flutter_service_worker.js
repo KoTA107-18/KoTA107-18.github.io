d292b114f2e13e72075a19e5cca20aed7fd87ce9
Andi Fauzy Dewantara <andifauzy00@gmail.com>
2021-05-07 17:36:11 +0700

    Update Bloc Poliklinik Done

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -14,9 +14,9 @@ const RESOURCES = {
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"index.html": "94e672078a9470df398599c7a45390c8",
-"/": "94e672078a9470df398599c7a45390c8",
-"main.dart.js": "3856e2b87c02ae30efd009379350d29b",
+"index.html": "54e60370b7c5ef6feaccb950187672ec",
+"/": "54e60370b7c5ef6feaccb950187672ec",
+"main.dart.js": "c615a7dc5b4630155caec1c0ba6b7de9",
 "manifest.json": "be75a275105cf347e753af0d81c9c56c",
 "version.json": "e3fc9c2a3090710cf54b08f1564fb1f4"
 };
