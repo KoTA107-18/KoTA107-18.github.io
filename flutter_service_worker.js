52f29237c9e8adcd9ccab138782e9746f1432e88
Andi Fauzy Dewantara <andifauzy00@gmail.com>
2021-06-05 19:18:26 +0700

    Update fitur Perawat

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -14,9 +14,9 @@ const RESOURCES = {
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"index.html": "c409e15d69660e45024cd3f112b9fdb8",
-"/": "c409e15d69660e45024cd3f112b9fdb8",
-"main.dart.js": "90acfd6dba71202e28ccd3f07ae46b76",
+"index.html": "09c8ef95876bcc67cc53f3a3cb4abc5c",
+"/": "09c8ef95876bcc67cc53f3a3cb4abc5c",
+"main.dart.js": "27e9211bb21d85673b74636961fcb0d5",
 "manifest.json": "be75a275105cf347e753af0d81c9c56c",
 "version.json": "e3fc9c2a3090710cf54b08f1564fb1f4"
 };
